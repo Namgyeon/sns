4198aeebaa6d5a594772bb2d7ce132222c739757
Namgyeon <getam101@naver.com>
2025-10-30 17:41:29 +0900

    Section03 완료 zustand

---
+++ src/components/todo-list/todo-editor.tsx
@@ -0,0 +1,27 @@
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useCreateTodo } from "@/store/todos";
+import { useState } from "react";
+
+export default function TodoEditor() {
+  const [content, setContent] = useState("");
+  const createTodo = useCreateTodo();
+
+  const handleAddClick = () => {
+    if (!content.trim()) return;
+
+    createTodo(content.trim());
+    setContent("");
+  };
+
+  return (
+    <div className="flex gap-2">
+      <Input
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+        placeholder="새로운 할 일을 입력하세요 ..."
+      />
+      <Button onClick={handleAddClick}>추가</Button>
+    </div>
+  );
+}
